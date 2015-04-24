36a9dcdaed1b1a30b091f4c973aa0f7c6170943b
Jihann <1244098192@qq.com>
2015-04-24 14:56:59 +0800

    add Class for js

---
+++ scripts/Class/mode1.js
@@ -0,0 +1,24 @@
+/**
+ * Created by JBH on 2015/4/24.
+ */
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+var p1 = new Person('Jihann', 22);
+console.log(p1.name);
+console.log(p1.age);
+
+console.log('---------------------------------------');
+Person.prototype.sex = 'boy';
+Person.prototype.say = function() {
+    console.log('--------------- this is me ---------------');
+    console.log('My name is ' + this.name + ", I am " + this.age + " years old this year, is a " + this.sex);
+};
+
+var p2 = new Person('ZhangSan', 18);
+console.log(p2.name);
+console.log(p2.age);
+console.log(p2.sex);
+console.log(p2.say());
